9ba6ca435a835a85b2cc90e202579b79a1072f38
joshua530 <joshuaomari5@gmail.com>
2022-08-06 18:30:41 +0300

    restore field positions

--- models/base_model.py
+++ models/base_model.py
@@ -30,8 +30,8 @@ class BaseModel:
                         setattr(self, k, v)
         else:
             self.id = str(uuid.uuid4())
-            self.updated_at = datetime.datetime.now()
             self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
             models.storage.new(self)
 
     def __str__(self):
